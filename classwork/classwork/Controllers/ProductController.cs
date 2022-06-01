1b56edc942aa48602bdf112c2c6c442060de2896
sazzadulalambd <Sazzad.sua@gmail.com>
2022-06-01 23:26:04 +0600

    first commit

---
+++ classwork/classwork/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using classwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace classwork.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = new List<Product>();
+            for (int i = 0; i < 10; i++)
+            {
+                products.Add(new Product()
+                    { 
+                    Id = (i + 1),
+                    Name = "Product " + (i + 1),
+                    Price = 599
+                    }
+                );
+            }
+            return View(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Product Pro)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(Pro);
+        }
+    }
+}
---
+++ classwork/classwork/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace classwork.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Enter Product ID")]
+        
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Product name")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Enter Product price")]
+        public int Price { get; set; }
+    }
+}
